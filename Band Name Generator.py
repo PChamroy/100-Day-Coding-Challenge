9adf2222d185ecbc078ade09313b0475507a1dc2
PChamroy <103671143+PChamroy@users.noreply.github.com>
2023-06-24 19:44:07 +0530

    Create Band Name Generator.py

---
+++ Band Name Generator.py
@@ -0,0 +1,8 @@
+print("Band Name Generator")
+print("Let's create a random band name based on your favourite city and pet")
+name = input("To start with, what's your name?\n")
+city_name = input("Hey "+name+"! What's your favourite city?\n")
+print("That's great!")
+animal_name = input("What's your favourite animal?\n")
+print("That's lovely! Let's generate your new band name now.")
+print("Your new band name is.......\n" + city_name + " " + animal_name)
